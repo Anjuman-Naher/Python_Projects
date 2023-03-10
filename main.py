2f15808e35f08bb366ac1f0b8ec9643e887427b9
Anjuman Naher Jui <113792564+Anjuman-Naher@users.noreply.github.com>
2023-03-10 21:01:06 +0600

    Ultimate_ massage

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-
-from tkinter import *
-from tkinter.ttk import *
-import datetime
-
-root = Tk()
-root.title("Clock")
-
-def clock():
-    date_time: str = datetime.datetime.now().strftime("%A,%d-%b-%Y")
-    date_label.config(text=date_time)
-    date_time: str = datetime.datetime.now().strftime("%I:%M:%S %p")
-    time_label.config(text=date_time)
-    time_label.after(1000, clock)
-
-time_label = Label(root, font=("ds-digital", 60), background="white", foreground="black")
-time_label.pack(anchor="center")
-date_label = Label(root, font=("Chivo-Mono", 40), background="white", foreground="black")
-date_label.pack(anchor="center")
-
-
-
-clock()
-
-mainloop()
+import pyautogui
+import time
+message = 5
+while message > 0:
+    time.sleep(2)
+    pyautogui.typewrite("Thanks for teach me something new")
+    time.sleep(1)
+    pyautogui.press("enter")
+    message = message - 1
